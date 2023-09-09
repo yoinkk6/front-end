d3d82c1b3b3cd3cd46620f0f919fe8b8ebae11f0
Surya-7890 <suryamariappan9108@gmail.com>
2023-09-09 06:49:44 +0530

    updated navbar menu

--- src/components/navbar/views/Mobile.jsx
+++ src/components/navbar/views/Mobile.jsx
@@ -5,10 +5,10 @@ function Mobile() {
   const [open, setOpen] = createSignal(true);
   return (
     <div onClick={() => setOpen(prev => !prev)} class="h-full w-[60%] lg:hidden flex">
-    <div class="h-[90%] w-[100%] flex flex-col justify-center space-y-2 items-end relative" classList={{ " space-y-0 ": open() }}>
-      <div class="h-[5%] transition-transform duration-500 ease-out aspect-[12/1] bg-black rounded-md" classList={{ " rotate-[-45deg] ": open()  }}></div>
+    <div class="h-[90%] w-[100%] flex flex-col justify-center space-y-[5%] items-end relative" classList={{ " space-y-0 ": open() }}>
+      <div class="h-[5%] transition-transform duration-500 ease-out aspect-[12/1] bg-black rounded-md" classList={{ " rotate-[-45deg] relative top-[6.25%] ": open()  }}></div>
       <div class="h-[5%] transition-transform duration-500 ease-out aspect-[12/1] bg-black rounded-md" classList={{ " hidden ": open()  }}></div>
-      <div class="h-[5%] transition-transform duration-500 ease-out aspect-[12/1] bg-black rounded-md" classList={{ " rotate-[45deg] relative top-[-12.5%] ": open()  }}></div>
+      <div class="h-[5%] transition-transform duration-500 ease-out aspect-[12/1] bg-black rounded-md" classList={{ " rotate-[45deg] relative top-[-6.25%] ": open()  }}></div>
     </div>
     <MobileList open={open} />
     </div>
