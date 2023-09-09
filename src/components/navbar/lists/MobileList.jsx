9f1f1eb4bab097debf848f1dc3639607ba101808
Surya-7890 <suryamariappan9108@gmail.com>
2023-09-09 09:40:50 +0530

    updated navbar

--- src/components/navbar/lists/MobileList.jsx
+++ src/components/navbar/lists/MobileList.jsx
@@ -4,16 +4,16 @@ import Home from "../../../svg/navbar/Home"
 
 function MobileList({ open }) {
   return (
-    <div class="h-[20%] absolute flex flex-col justify-between transition-[width] duration-500 ease-out top-[10%] overflow-hidden right-0" classList={{ " w-[40%] ": open(), " w-0 ": !open() }}>
-      <div class="flex items-center justify-between px-2 overflow-hidden">
+    <div class="h-[20%] absolute flex flex-col justify-between transition-[width] duration-500 ease-out top-[10%] overflow-hidden right-0" classList={{ " w-[35%] ": open(), " w-0 ": !open() }}>
+      <div class="flex items-center justify-start space-x-2 px-2 overflow-hidden">
         <Home/>
         <div classList={{ "hidden": !open() }}>Home</div>
       </div>
-      <div class="flex items-center justify-between px-2 overflow-hidden">
+      <div class="flex items-center justify-start space-x-2 px-2 overflow-hidden">
         <AboutUs/>
         <div classList={{ "hidden": !open() }}>About Us</div>
       </div>
-      <div class="flex items-center justify-between px-2 overflow-hidden">
+      <div class="flex items-center justify-start space-x-2 px-2 overflow-hidden">
         <Gallery/>
         <div classList={{ "hidden": !open() }}>Gallery</div>
       </div>
